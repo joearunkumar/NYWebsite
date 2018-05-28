58ceb4a2e3ba2f015ffa27c0bc78e7712dc1dcdd
Joe Arun Kumar <joearun.1990@gmail.com>
2018-05-28 17:02:14 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "joearunkumaramalraj.name"
+  },
+  apiGateway: {
+    REGION: "YOUR_API_GATEWAY_REGION",
+    URL: "YOUR_API_GATEWAY_URL"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_nEixfhNQF",
+    APP_CLIENT_ID: "7ctk2ekrajoms9bnbf1na2ca4t",
+    IDENTITY_POOL_ID: "us-east-1:07a2e955-f924-4076-a01f-cb390b5162cd"
+  }
+};
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>NenjaeYezhu</h1>
+          <p>A community for service</p>
+        </div>
+      </div>
+    );
+  }
+}
