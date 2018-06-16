7d0eb98977d332b8271f2d50c348e034b2a3667f
Joe Arun Kumar Amalraj <joearun.1990@gmail.com>
2018-06-16 19:16:19 +0530

    Added display name after login

--- src/containers/MemberHome.js
+++ src/containers/MemberHome.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import "./MemberHome.css";
+import { Auth } from "aws-amplify";
 
 export default class MemberHome extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: ""
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      var userInfo = await Auth.currentUserInfo();
+      this.state.userName = userInfo.attributes.name;
+    } catch (err) {
+      console.log('error fetching user info: ', err);
+    }
+  }
+
   render() {
     return (
       <div className="MemberHome">
         <div className="lander">
-          <h1>Welcome</h1>
-          //{this.prop.name}
+          <h1>Welcome {this.state.userName}</h1>
           <p></p>
         </div>
       </div>
