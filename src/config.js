87c4a1a3f25b8a126f594c05b0f9cea62e912283
Joe Arun Kumar Amalraj <32150840+joearunkumar@users.noreply.github.com>
2018-06-16 19:09:39 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,16 +1,8 @@
 export default {
-  s3: {
-    REGION: "us-east-1",
-    BUCKET: "joearunkumaramalraj.name"
-  },
-  apiGateway: {
-    REGION: "YOUR_API_GATEWAY_REGION",
-    URL: "YOUR_API_GATEWAY_URL"
-  },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_5EVV9ZfNA",
-    APP_CLIENT_ID: "4fqmb7c2gghlul954ca4v1bi5a",
-    IDENTITY_POOL_ID: "us-east-1:ab024844-e707-491a-9c85-2682928b4595"
+    REGION: "REGION",
+    USER_POOL_ID: "USER_POOL_ID",
+    APP_CLIENT_ID: "APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "IDENTITY_POOL_ID"
   }
 };
